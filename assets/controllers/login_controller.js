9c66ba42169a1f095c92d2600654d2d18d3287eb
Erick Hernandez <takashi.onimaru@gmail.com>
2023-10-09 22:13:20 -0400

    Validacion de login con mensajes traducidos

--- assets/controllers/login_controller.js
+++ assets/controllers/login_controller.js
@@ -2,7 +2,13 @@ import { Controller } from 'stimulus';
 import $ from 'jquery';
 
 export default class extends Controller { 
+    static values = {
+        errorEmailMessage: String,
+        errorPassMessage: String,
+    }
     connect() { 
+        const errorEmailMessage = this.errorEmailMessageValue;
+        const errorPassMessage = this.errorPassMessageValue;
         const passSaved = localStorage.getItem("pass_remember");
         const userSaved = localStorage.getItem("user_remember");
         $("#password").val(passSaved);
@@ -10,23 +16,25 @@ export default class extends Controller {
         
         $('form').on('submit', function(e){
             const pass = $("#password").val();
+            const passError = $("#password").parent().children('.invalid-feedback');
             const user = $("#username").val();
+            const userError = $("#username").parent().children('.invalid-feedback');
             const remember = $("#remember_me").is(':checked');
             if (!user) {
-                $("#username").css('border-color', '#ed2000')
+                $("#username").css('border-color', '#ed2000');
+                userError.html(errorEmailMessage);
+                userError.fadeIn();
                 e.preventDefault();
             } else {
-                $("#username").css('border-color', '#cbd0dd')
+                $("#username").css('border-color', '#cbd0dd');
             }
             if (!pass) {
-                $("#password").css('border-color', '#ed2000')
+                $("#password").css('border-color', '#ed2000');
+                passError.html(errorPassMessage);
+                passError.fadeIn();
                 e.preventDefault();
             } else {
-                $("#password").css('border-color', '#cbd0dd')
-            }
-
-            if (!user || !pass) {
-                $('#alert-warning').html('Los campos marcados en rojo son requeridos').fadeIn()
+                $("#password").css('border-color', '#cbd0dd');
             }
 
             if(remember){
