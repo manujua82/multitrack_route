dc84514ab3836c621e2474bdf32f1562eec73591
Juan Manuel Salcedo <manujua@gmail.com>
2023-10-26 18:06:29 -0400

    Notificaciones Setup v1 (#66)

--- src/Entity/OrderStatus.php
+++ src/Entity/OrderStatus.php
@@ -6,6 +6,9 @@ enum OrderStatus: string
 {
     case UNSCHEDULE = "unschedule";
     case SCHEDULE= "schedule";
+    case IN_PROGRESS="inProgress";
     case DELIVERED = "delivered";
     case REJECTED = "rejected";
+    case PARTIALLY = "partially";
+    case NO_DELIVERED = "noDelivered";
 }
